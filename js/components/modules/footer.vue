a3aedfa7e2bb3ee09419812a2a6ebbc42e7c5fa5
TakaoHoshino <tk.1987.6.9@gmail.com>
2020-03-07 23:29:26 +0900

    add

---
+++ js/components/modules/footer.vue
@@ -0,0 +1,7 @@
+const app = new Vue({
+    el: '#app',
+
+    data: {
+        testVal: 'Hello World!' //testValを定義
+    }
+});
---
+++ js/index.js
@@ -0,0 +1,9 @@
+var vue = require('vue');
+// var header = require('./components/header.vue');
+// var footer = require('./components/footer.vue');
+// // console.log(header.fuga);
+// // console.log(footer.hoge);
+var app1 = new Vue({
+  el: '#test-app',                        /* #app-1 要素に対して Vue を適用する */
+  data: { message: 'Hello world!' }    /* message という名前のデータを定義する */
+})
